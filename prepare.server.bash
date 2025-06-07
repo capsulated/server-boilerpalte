66dbddcd14e28405ff99b591777000325675ed8a
Nikita Romanov <architect@logiq.one>
2025-06-07 20:30:09 +0300

    remove sudo from package installation commands in prepare.server.bash

--- prepare.server.bash
+++ prepare.server.bash
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 # apt update
-sudo apt -y update && apt -y upgrade
+apt -y update && apt -y upgrade
 
 # ssh
 ssh-keygen -t ed25519 -C 'arhitect@logiq.one'
 
 # git
-sudo apt -y install git-all curl neovim
+apt -y install git-all curl neovim
 
 # docker
-sudo apt -y install ca-certificates curl gnupg
+apt -y install ca-certificates curl gnupg
 
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
  tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt update
-sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt update
+apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # zsh + oh my zsh
-sudo apt install zsh
+apt install zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
